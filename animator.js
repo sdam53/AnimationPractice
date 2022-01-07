8fe118925b4771951a558bb235110338d60036f1
Shilnara Dam <ShilnaraWDam@gmail.com>
2022-01-07 03:25:32 -0800

    added a frame and flag variable to know the animation's current frame and whether it has completed.

--- animator.js
+++ animator.js
@@ -1,9 +1,11 @@
 class Animator {
-    constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration, framePadding, reverse, loop) {
-        Object.assign(this, { spritesheet, xStart, yStart, height, width, frameCount, frameDuration, framePadding, reverse, loop });
+    constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration, framePadding, reverse, loop, player) {
+        Object.assign(this, { spritesheet, xStart, yStart, height, width, frameCount, frameDuration, framePadding, reverse, loop, player });
 
         this.elapsedTime = 0;
         this.totalTime = this.frameCount * this.frameDuration;
+        this.frame = 1; //gives the frame of the current animations //will use this to update BB for attack animation
+        this.flag = false;
 
     };
 
@@ -12,21 +14,26 @@ class Animator {
 
         if (this.isDone()) {
             if (this.loop) {
+                this.frame = 1; //means animation finished
+                this.flag = false;
                 this.elapsedTime -= this.totalTime;
             } else {
                 return;
             }
         }
+        //console.log(this.flag);
 
-        let frame = this.currentFrame();
-        if (this.reverse) frame = this.frameCount - frame - 1;
+        this.frame = this.currentFrame();
+        if (this.reverse) this.frame = this.frameCount - frame - 1;
 
         ctx.drawImage(this.spritesheet,
-            this.xStart + frame * (this.width + this.framePadding), this.yStart, //source from sheet
+            this.xStart + this.frame * (this.width + this.framePadding), this.yStart, //source from sheet
             this.width, this.height,
             x, y,
             this.width * scale,
             this.height * scale);
+            
+        this.frame++;
     };
 
     currentFrame() {
