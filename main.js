f96b56c5b181fc0c55cb0a8ebc2d57ec0657a322
Shilnara Dam <ShilnaraWDam@gmail.com>
2022-01-19 19:38:19 -0800

    queued background assets and music

--- main.js
+++ main.js
@@ -8,9 +8,24 @@ ASSET_MANAGER.queueDownload("./assets/playerright.png");
 ASSET_MANAGER.queueDownload("./assets/leftteleport.png");
 ASSET_MANAGER.queueDownload("./assets/rightteleport.png");
 ASSET_MANAGER.queueDownload("./assets/ui.png");
+ASSET_MANAGER.queueDownload("./assets/Background_1.png");
+ASSET_MANAGER.queueDownload("./assets/Background_0.png");
+ASSET_MANAGER.queueDownload("./assets/tileset 1.png");
 
+ASSET_MANAGER.queueDownload("./assets/test/player_front_death.png");
+ASSET_MANAGER.queueDownload("./assets/test/player_front_idle.png");
+ASSET_MANAGER.queueDownload("./assets/test/player_front_run.png");
+ASSET_MANAGER.queueDownload("./assets/test/player_left_idle.png");
+ASSET_MANAGER.queueDownload("./assets/test/player_left_run.png");
+ASSET_MANAGER.queueDownload("./assets/test/player_right_idle.png");
+ASSET_MANAGER.queueDownload("./assets/test/player_right_run.png");
+
+//music
+ASSET_MANAGER.queueDownload("./assets/alex-productions-epic-cinematic-gaming-cyberpunk-reset.mp3");
 
 ASSET_MANAGER.downloadAll(() => {
+	ASSET_MANAGER.autoRepeat("./assets/alex-productions-epic-cinematic-gaming-cyberpunk-reset.mp3");
+
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 	ctx.imageSmoothingEnabled = false;
