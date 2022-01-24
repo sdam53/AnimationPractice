cec39da27870ac8089b3cc9169993f28552f4ec7
Shilnara Dam <ShilnaraWDam@gmail.com>
2022-01-23 16:53:03 -0800

    fixed sprite bounds

--- shadowofstorms.js
+++ shadowofstorms.js
@@ -23,19 +23,19 @@ class ShadowOfStorms {
 
   loadAnimations() {
     //shootgun
-    this.animations[0] = new Animator(this.spriteSheet, 0, 0, 136,90,12,.2,0,false,true);
+    this.animations[0] = new Animator(this.spriteSheet, 0, 0, 137,90,12,.2,0,false,true);
     //wwalk with gun
-    this.animations[1] = new Animator(this.spriteSheet, 1632, 0, 136,90,6,.2,0,false,true);
+    this.animations[1] = new Animator(this.spriteSheet, 1632, 0, 137,90,6,.2,0,false,true);
     //attack1
-    this.animations[2] = new Animator(this.spriteSheet, 2448, 0, 136,90,22,.2,0,false,true);
+    this.animations[2] = new Animator(this.spriteSheet, 2448, 0, 137,90,22,.2,0,false,true);
     //attack2
-    this.animations[3] = new Animator(this.spriteSheet, 5440, 0, 136,90,10,.2,0,false,true);
+    this.animations[3] = new Animator(this.spriteSheet, 5440, 0, 137,90,10,.2,0,false,true);
     //heal
-    this.animations[4] = new Animator(this.spriteSheet, 6800, 0, 136,90,8,.2,0,false,true);
+    this.animations[4] = new Animator(this.spriteSheet, 6800, 0, 137,90,8,.2,0,false,true);
     //damage
-    this.animations[5] = new Animator(this.spriteSheet, 7888, 0, 136,90,2,.2,0,false,true);
+    this.animations[5] = new Animator(this.spriteSheet, 7888, 0, 137,90,2,.2,0,false,true);
     //death
-    this.animations[6] = new Animator(this.spriteSheet, 8160, 0, 136,90,7,.2,0,false,true);
+    this.animations[6] = new Animator(this.spriteSheet, 8160, 0, 137,90,7,.2,0,false,true);
   }
 
 
